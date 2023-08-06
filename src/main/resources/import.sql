3661be6369b59fb5e54f523cd6401b70d357fc34
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-08-06 18:31:50 -0300

    feat: :sparkles: adicionando conexão com banco de dados local H2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informática');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@mail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@mail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex', 'alex@mail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@mail.com');
