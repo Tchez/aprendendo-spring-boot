e2d9206ea090ddbd79d387e63b007bb2b33fdd3f
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-08-06 20:16:45 -0300

    feat: :sparkles: adicionando endpoints para consulta e criação de um usuário

--- src/main/java/com/example/userdept/controllers/UserController.java
+++ src/main/java/com/example/userdept/controllers/UserController.java
@@ -1,14 +1,18 @@
 package com.example.userdept.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.userdept.entities.User;
 import com.example.userdept.repositories.UserRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -22,4 +26,17 @@ public class UserController {
         List<User> result = repository.findAll();
         return result;
     }
+
+    @GetMapping(value = "/{id}")
+    public Optional<User> getUser(@PathVariable Long id) {
+        Optional<User> user = repository.findById(id);
+        return user;
+    }
+
+    @PostMapping
+    public User postUser(@RequestBody User user) {
+        User result = repository.save(user);
+        return result;
+    }
+
 }
